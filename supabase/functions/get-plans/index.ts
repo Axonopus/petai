0db6cbedd29fa12fcfce501b9f3cefe4cb669c60
Shifa <shifa@petotum.com>
2025-03-12 14:31:01 +0800

    deployment bug fix

--- supabase/functions/get-plans/index.ts
+++ supabase/functions/get-plans/index.ts
@@ -26,7 +26,7 @@ export default async function handler(
 
   try {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-      apiVersion: "2025-01-27.acacia",
+      apiVersion: "2025-02-24.acacia",
     });
 
     const plans = await stripe.plans.list({
